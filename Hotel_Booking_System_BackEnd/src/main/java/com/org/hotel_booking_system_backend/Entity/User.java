fcb58502c0b0943a69c63034e7109f2be8cb1e9d
Nethu <nilakshinethmi48@gmail.com>
2025-03-30 11:44:05 +0530

    frontend

--- Hotel_Booking_System_BackEnd/src/main/java/com/org/hotel_booking_system_backend/Entity/User.java
+++ Hotel_Booking_System_BackEnd/src/main/java/com/org/hotel_booking_system_backend/Entity/User.java
@@ -16,7 +16,7 @@ public class User {
     @Id
     private String userId;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String name;
 
     @Column(nullable = false, unique = true)
